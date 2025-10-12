8ae920ea4875bc806da1f99aad8fd0b53c3d23a6
sahilgupta20 <49154507+sahilgupta20@users.noreply.github.com>
2025-10-12 14:41:16 +0530

    notification change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,7 @@ export default function RootLayout({
             `,
           }}
         />
+        {children}
         <Toaster position="top-right" richColors expand={true} />
       </body>
     </html>
